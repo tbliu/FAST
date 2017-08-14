e833b2bf436a6196dbd5a6a3c9593e5ba0db17af
Timothy Liu <tliu12@berkeley.edu>
2017-08-14 11:38:26 -0700

    bug fixes; works for noisy images now

--- src/fast.py
+++ src/fast.py
@@ -5,6 +5,11 @@ Reference:
     - http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_fast/py_fast.html
 """
 
+
+""" 
+***** Begin NumPy and OpenCV functions. ******
+"""
+
 def shape(array):
     """ 
     Returns a list of 2D array dimensions 
@@ -14,18 +19,60 @@ def shape(array):
     cols = len(array[0])
     return [rows, cols]
 
+def zeros(rows, cols):
+    """
+    Returns a 2D array of all 0's
+    """
+    return [[0 for col in range(cols)] for row in range(rows)]
+
 def rgb2gray(array):
     """
     Transforms RGB image matrix into grayscale. 
     Uses formula from: https://en.wikipedia.org/wiki/Grayscale#Converting_color_to_grayscale
     """
     rows, cols = shape(array)
+    grayscale = zeros(rows, cols)
     for row in range(rows):
         for col in range(cols):
             red, green, blue = array[row][col]
             gray = int(0.3*red + 0.59*green + 0.11*blue)
-            array[row][col] = gray
+            grayscale[row][col] = gray
+    return grayscale
 
+def medianBlur(image, startSearchRow, endSearchRow, startSearchCol, endSearchCol, N=3):
+    """
+    Performs median blur on image to remove severe salt and pepper noise.
+    Median blur replaces each pixel with the median of the NxN pixels surrounding it.
+    N must be an odd integer.
+
+    In order to increase performnace, this function only applies median blur to the 
+    search area, not the entire image.
+    """
+    dst = image[:] 
+    for y in range(startSearchRow, endSearchRow):
+        for x in range(startSearchCol, endSearchCol):
+            window = []
+            for i in range(y - N // 2, y + N // 2 + 1):
+                for j in range(x - N//2, x + N//2 + 1):
+                    window.append(image[i][j])
+            insertionSort(window)
+            dst[y][x] = window[len(window)//2]
+
+    return dst
+    
+def insertionSort(lst):
+    for index in range(1, len(lst)):
+        currentvalue = lst[index]
+        position = index
+
+        while position > 0 and lst[position - 1] > currentvalue:
+            lst[position] = lst[position - 1]
+            position = position - 1
+
+        lst[position] = currentvalue
+"""
+***** Begin helper functions for FAST *****
+"""
 
 def circle(row, col):
     """ 
@@ -71,7 +118,46 @@ def is_corner(image, row, col, ROI, threshold):
     but it runs much faster and we can set the threshold to be a high value to filter
     out more non-corners
     """
-    intensity = int(image[row][col]) # Center pixel intensity
+    intensity = int(image[row][col])
+    row1, col1 = ROI[0]
+    row9, col9 = ROI[4]
+    row5, col5 = ROI[2]
+    row13, col13 = ROI[6]
+    intensity1 = int(image[row1][col1])
+    intensity9 = int(image[row9][col9])
+    intensity5 = int(image[row5][col5])
+    intensity13 = int(image[row13][col13])
+    count = 0
+    if abs(intensity1 - intensity) > threshold:
+        count += 1 
+    if abs(intensity9 - intensity) > threshold:
+        count += 1
+    if abs(intensity5 - intensity) > threshold:
+        count += 1
+    if abs(intensity13 - intensity) > threshold:
+        count += 1
+
+    return count >= 3
+
+def areAdjacent(point1, point2):
+    """
+    Identifies if two points are adjacent by calculating distance in terms of rows/cols
+    Two points are adjacent if they are within four pixels of each other (Euclidean distance)
+    """
+    row1, col1 = point1
+    row2, col2 = point2
+    xDist = row1 - row2
+    yDist = col1 - col2
+    return (xDist ** 2 + yDist ** 2) ** 0.5 <= 4
+
+def calculateScore(image, point, ROI):
+    """ 
+    Calculates the score for non-maximal suppression. 
+    The score V is defined as the sum of the absolute difference between the intensities of 
+    all points returned by the circle function and the intensity of the center pixel.
+    """
+    col, row = point
+    intensity = int(image[row][col])
     row1, col1 = ROI[0]
     row3, col3 = ROI[1]
     row5, col5 = ROI[2]
@@ -87,94 +173,14 @@ def is_corner(image, row, col, ROI, threshold):
     intensity9 = int(image[row9][col9])
     intensity11 = int(image[row11][col11])
     intensity13 = int(image[row13][col13])
-    intensity15 = int(image[row15][col15])
-
-    if abs(intensity1 - intensity) > threshold:
-        if abs(intensity15 - intensity) > threshold and abs(intensity3 - intensity) > threshold:
-            if abs(intensity13 - intensity) > threshold and abs(intensity5 - intensity) > threshold:
-                return True
-    
-    if abs(intensity13 - intensity) > threshold:
-        if abs(intensity15 - intensity) > threshold and abs(intensity11 - intensity) > threshold:
-            if abs(intensity1 - intensity) > threshold and abs(intensity9 - intensity) > threshold:
-                return True
-
-    if abs(intensity9 - intensity) > threshold:
-        if abs(intensity11 - intensity) > threshold and abs(intensity7 - intensity) > threshold:
-            if abs(intensity13 - intensity) > threshold and abs(intensity5 - intensity) > threshold:
-                return True
-
-    if abs(intensity5 - intensity) > threshold:
-        if abs(intensity3 - intensity) > threshold and abs(intensity7 - intensity) > threshold:
-            if abs(intensity1 - intensity) > threshold and abs(intensity9 - intensity) > threshold:
-                return True
-    
-    if abs(intensity3 - intensity) > threshold:
-        if abs(intensity1 - intensity) > threshold and abs(intensity5 - intensity) > threshold:
-            if abs(intensity15 - intensity) > threshold and abs(intensity7 - intensity) > threshold:
-                return True
-
-    if abs(intensity7 - intensity) > threshold:
-        if abs(intensity5 - intensity) > threshold and abs(intensity9 - intensity) > threshold:
-            if abs(intensity3 - intensity) > threshold and abs(intensity11 - intensity) > threshold:
-                return True
-
-    if abs(intensity11 - intensity) > threshold:
-        if abs(intensity13 - intensity) > threshold and abs(intensity9 - intensity) > threshold:
-            if abs(intensity7 - intensity) > threshold and abs(intensity15 - intensity) > threshold:
-                return True
-
-    if abs(intensity15 - intensity) > threshold:
-        if abs(intensity13 - intensity) > threshold and abs(intensity1 - intensity) > threshold:
-            if abs(intensity11 - intensity) > threshold and abs(intensity3 - intensity) > threshold:
-                return True
-
-    return False
-
-def areAdjacent(point1, point2):
-    """
-    Identifies if two points are adjacent by calculating distance in terms of rows/cols
-    Two points are adjacent if they are within four pixels of each other (Euclidean distance)
-    """
-    row1, col1 = point1
-    row2, col2 = point2
-    xDist = row1 - row2
-    yDist = col1 - col2
-    return pow(pow(xDist, 2) + pow(yDist, 2), 0.5) <= 4
-
-def calculateScore(image, point):
-    """ 
-    Calculates the score for non-maximal suppression. 
-    The score V is defined as the sum of the absolute difference between the intensities of 
-    all points returned by the circle function and the intensity of the center pixel.
-    """
-    row, col = point
-    intensity = int(image[row][col])
-    
-    intensity1 = image[row+3][col] 
-    
-    intensity3 = image[row+3][col-1] 
-    
-    intensity5 = image[row+1][col+3]    
-    
-    intensity7 = image[row-1][col+3]
-    
-    intensity9 = image[row-3][col] 
-    
-    intensity11 = image[row-3][col-1] 
-    
-    intensity13 = image[row+1][col-3] 
-    
-    intensity15 = image[row-1][col-3]
-       
+    intensity15 = int(image[row15][col15])   
     score = abs(intensity - intensity1) + abs(intensity - intensity3) + \
             abs(intensity - intensity5) + abs(intensity - intensity7) + \
             abs(intensity - intensity9) + abs(intensity - intensity11) + \
             abs(intensity - intensity13) + abs(intensity - intensity15)
     return score
 
-
-def suppress(image, corners):
+def suppress(image, corners, ROI):
     """
     Performs non-maximal suppression on the list of corners.
     For adjacent corners, discard the one with the smallest score.
@@ -190,11 +196,10 @@ def suppress(image, corners):
         currPoint = corners[i]
         prevPoint = corners[i - 1]
         if areAdjacent(prevPoint, currPoint):
-            currScore = calculateScore(image, currPoint)
-            prevScore = calculateScore(image, prevPoint)
+            currScore = calculateScore(image, currPoint, ROI)
+            prevScore = calculateScore(image, prevPoint, ROI)
             if (currScore > prevScore):
                 del(corners[i - 1])
-                i += 1
             else:
                 del(corners[i])
         else:
@@ -202,7 +207,11 @@ def suppress(image, corners):
             continue
     return
 
-def detect(image, threshold=100):
+"""
+***** Begin core FAST algorithm *****
+"""
+
+def detect(image, threshold=50):
     """
     corners = fast.detect(image, threshold) performs the detection
     on the image and returns the corners as a list of (x,y) tuples
@@ -217,19 +226,21 @@ def detect(image, threshold=100):
         image is a numpy array of intensity values. NOTE: Image must be grayscale
         threshold is an int used to filter out non-corners. 
     """
-
+    # Initialization
+    image = rgb2gray(image)
     corners = []
-    imshape = shape(image)
-    rows = imshape[0]
-    cols = imshape[1]
+    rows,cols = shape(image)
     startSearchRow = int(0.25*rows)
     endSearchRow = int(0.75*rows) # search the middle square of the frame
     startSearchCol = int(0.25*cols)
     endSearchCol = int(0.75*cols)
+    image = medianBlur(image, startSearchRow, endSearchRow, startSearchCol, endSearchCol)
+
+    # Begin searching through search area
     for row in range(startSearchRow, endSearchRow):
         for col in range(startSearchCol, endSearchCol):
             ROI = circle(row, col) 
             if is_corner(image, row, col, ROI, threshold):
                 corners.append((col, row))
-    #suppress(image, corners) 
+    suppress(image, corners, ROI) 
     return corners;
--- src/test.py
+++ src/test.py
@@ -3,18 +3,43 @@ from matplotlib import pyplot as plt
 import cv2
 import numpy as np
 def test():
-    #image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/ansel.jpg');
-    image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/noisy.png');
-    #image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/chessboard.jpg');
-    image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/balloons_noisy.png');
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    #rgb2gray(image)
-    kernel = np.ones((5,5),np.float32)/25
-    cv2.filter2D(image,-1,kernel) 
+    #image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/noisy.png')
+    image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/balloons_noisy.png')
+    #image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/lena.png')
+    imgray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    imgray = cv2.medianBlur(imgray, 7)
     corners = detect(image)
-    implot = plt.imshow(image, cmap='gray')
+    implot = plt.imshow(imgray, cmap='gray')
     for point in corners:
         plt.scatter(point[0], point[1], s=10)
     plt.show()
 
+def testMedianBlur():
+    #image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/lena.png')
+    image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/balloons_noisy.png')
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    rows, cols = shape(image)
+    startSearchRow = int(0.25*rows)
+    endSearchRow = int(0.75*rows)
+    startSearchCol = int(0.25*cols)
+    endSearchCol = int(0.75*cols)
+    image = medianBlur(image, startSearchRow, endSearchRow, startSearchCol, endSearchCol)
+    implot = plt.imshow(image, cmap='gray')
+    plt.show()
+
+def testgray():
+    image = cv2.imread('/Users/timmytimmyliu/research/odometry/test_images/balloons_noisy.png')
+    image = rgb2gray(image)
+    print(image[0][0])
+    implot = plt.imshow(image, cmap='gray')
+    plt.show()
+
+def testInsertionSort():
+    lst = [3,4,12,16,1, 0]
+    insertionSort(lst)
+    print(lst)
+
+#testInsertionSort()
+#testgray()
+#testMedianBlur()
 test()
